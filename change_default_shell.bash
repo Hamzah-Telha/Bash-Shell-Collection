7ee8d5581a60e13ff90be508586ee43d6b0b1d88
Hamzah Telha <50617242+Hamzah-Telha@users.noreply.github.com>
2023-07-24 18:10:27 +0300

    Update change_default_shell.bash for ubuntu

--- change_default_shell.bash
+++ change_default_shell.bash
@@ -3,9 +3,9 @@ read shell
 if [ $shell == 'b' ]
 then
     echo "change the default shell to bash"
-    exec bash
+    chsh -s $(which bash)
 elif [ $shell == 'z' ]
 then
     echo "change the default shell to zsh"
-    exec zsh
+    chsh -s $(which zsh)
 fi
